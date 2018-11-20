2a67ef16ea8da72953d85031d9349f9b612bebc7
XhemailRamabaja <xhemail.ramabaja@st.oth-regensburg.de>
2018-11-20 21:50:38 +0100

    Paddle with mousecontrol added

--- app/BreakOutGame.js
+++ app/BreakOutGame.js
@@ -8,72 +8,91 @@
  */
 var breakOutGame = (function () {
 
-	// private vars and constants
+    // private vars and constants
     var privateContext;
-	var privateCanvas;
-    
-	var GAME_WIDTH = 600;
-	var GAME_HEIGHT = 500;
-	var BRICK_ROWS = 5;
-	var BRICK_COLUMNS = 13;
-	var BALLSIZE = 10;
-	var BRICK_WIDTH = 40;
-	var BRICK_HEIGHT = 10;
-    
-	var bricks = [];
-	var paddle;
-	var ball;
-    
+    var privateCanvas;
+
+    var GAME_WIDTH = 600;
+    var GAME_HEIGHT = 500;
+    var BRICK_ROWS = 5;
+    var BRICK_COLUMNS = 13;
+    var BALLSIZE = 10;
+    var BRICK_WIDTH = 40;
+    var BRICK_HEIGHT = 10;
+
+    var bricks = [];
+    var paddle;
+    var ball;
+
     //Added variables
+    //BRICK
     var singleBrick;
     var spaceBetweenBricksX = 5;
     var spaceBetweenBricksY = 15;
-    var xPos = 10;
-    var yPos = 10;
-    
-	function privateDraw() {
-        for(var i = 0;i < bricks.length;i++){
+    var brickXPos = 10;
+    var brickYPos = 10;
+    //PADDLE
+    var paddleWidth = 60;
+    var paddleHeight = 10;
+    var paddleXPos;
+
+    function privateDraw() {
+        privateContext.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        for (var i = 0; i < bricks.length; i++) {
             bricks[i].draw();
         }
+        paddle.updateXPos(paddleXPos);
+        paddle.draw();
         window.requestAnimationFrame(privateDraw);
-	}
+    }
 
-	function privateSetContext(canvas) {
-		privateCanvas = canvas;
-		privateContext = canvas.getContext("2d");
-	}
+    function privateSetContext(canvas) {
+        privateCanvas = canvas;
+        privateContext = canvas.getContext("2d");
+    }
 
-	function publicInit(canvas, difficulty) {
-		privateSetContext(canvas);
-        //SET BRICKWALL
-        for(var i = 1;i <= BRICK_ROWS;i++){
-            xPos = 10;
-            for(var j = 1;j <= BRICK_COLUMNS;j++){
-                singleBrick = new Brick(privateContext,xPos,yPos,getRowColor(i),BRICK_WIDTH,BRICK_HEIGHT);
-                bricks.push(singleBrick);
-                xPos = xPos + BRICK_WIDTH + spaceBetweenBricksX;
-            }
-            yPos = yPos + spaceBetweenBricksY;
-        }
-        
-		window.requestAnimationFrame(privateDraw);
-	}
+    function publicInit(canvas, difficulty) {
+        privateSetContext(canvas);
+        setBrickWall();
+        paddle = new Paddle(privateContext, GAME_WIDTH, GAME_HEIGHT, paddleWidth, paddleHeight);
+        canvas.addEventListener('mousemove', updatePaddlePosition);
+        window.requestAnimationFrame(privateDraw);
+    }
     
-    function getRowColor(row){
-        if(row == 1 || row == 2){
-           return "red";
-        }else if(row == 3){
-           return "yellow";
-        }else if(row == 4){
-           return "orange";
-        }else if(row == 5){
-           return "green";
+    //COLORED BRICKWALL
+    function setBrickWall() {
+        for (var i = 1; i <= BRICK_ROWS; i++) {
+            brickXPos = 10;
+            for (var j = 1; j <= BRICK_COLUMNS; j++) {
+                singleBrick = new Brick(privateContext, brickXPos, brickYPos, getRowColor(i), BRICK_WIDTH, BRICK_HEIGHT);
+                bricks.push(singleBrick);
+                brickXPos = brickXPos + BRICK_WIDTH + spaceBetweenBricksX;
+            }
+            brickYPos = brickYPos + spaceBetweenBricksY;
+        }
+    }
+    //MOUSE LISTENER
+    function updatePaddlePosition(MouseEvent) {
+        paddleXPos = MouseEvent.clientX - canvas.offsetLeft;
+        console.log(paddleXPos);
+
+    }
+    //ROW COLORS
+    function getRowColor(row) {
+        if (row == 1 || row == 2) {
+            return "red";
+        } else if (row == 3) {
+            return "yellow";
+        } else if (row == 4) {
+            return "orange";
+        } else if (row == 5) {
+            return "green";
         }
     }
 
-	return {
-		init: publicInit
-	};
+    return {
+        init: publicInit
+    };
 })();
 
 var canvas = document.getElementById("breakoutcanvas");
--- app/gameobjects/Paddle.js
+++ app/gameobjects/Paddle.js
@@ -1,11 +1,22 @@
 /* Paddle represents the players paddle used to deflect the ball in the game */
 class Paddle {
-    conbstructor() {
+    constructor(context, canvasWidth, canvasHeight, paddleWidth, paddleHeight) {
+        this.context = context;
+        this.xPos = (canvasWidth / 2) - (paddleWidth / 2);
+        this.width = paddleWidth;
+        this.height = paddleHeight;
+        this.yPos = canvasHeight - 40;
     }
-    
+
     draw() {
+        this.context.fillStyle = "white";
+        this.context.fillRect(this.xPos, this.yPos, this.width, this.height);
+
     }
-    
-    updateXPos() {
+
+    updateXPos(xPos) {
+        if (xPos + this.width <= 600){
+            this.xPos = xPos;
+        }
     }
 }
